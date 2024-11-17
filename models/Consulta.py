4a952fb77487985b360f13c9a88219c0bc0ec132
amandatenor <83618366+amandatenor@users.noreply.github.com>
2024-11-17 23:44:41 +0000

    build: Cria pasta models e classes Armazenamento, Consulta e Paciente

---
+++ models/Consulta.py
@@ -0,0 +1,22 @@
+class Consulta:
+    def __init__(self, id_consulta, paciente, procedimentos, data_consulta):
+        self.id_consulta = id_consulta
+        self.paciente = paciente
+        self.procedimentos = procedimentos
+        self.data_consulta = data_consulta
+
+    def registrar(self):
+        
+        pass
+
+    def editar(self, novos_dados):
+        
+        pass
+
+    def excluir(self):
+        
+        pass
+
+    def consultar(self):
+        
+        pass
---
+++ models/Paciente.py
@@ -0,0 +1,22 @@
+class Paciente:
+    def __init__(self, id_paciente, nome, idade, genero):
+        self.id_paciente = id_paciente
+        self.nome = nome
+        self.idade = idade
+        self.genero = genero
+
+    def inserir(self):
+        
+        pass
+
+    def editar(self, novos_dados):
+        
+        pass
+
+    def excluir(self):
+        
+        pass
+
+    def consultar(self):
+        
+        pass
